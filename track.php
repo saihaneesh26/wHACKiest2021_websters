380e2a634ec9504bedc37a6925202939a1114b5f
saihaneesh26 <56464193+saihaneesh26@users.noreply.github.com>
2021-04-03 16:52:50 +0530

    Add files via upload

--- track.php
+++ track.php
@@ -44,8 +44,8 @@ $var=('<iframe src="https://maps.google.com/maps?q='.$json_file[$arval]['name'].
 <!DOCTYPE html>
 <html>
 <head>
-<link rel="stylesheet" href="style.css">
 	<title>Track nearby</title>
+	 <link rel="icon" href="favicon.ico" type="image/ico" >
 </head>
 <body  style="background-image: repeating-linear-gradient(135deg, rgba(86, 86, 86, 0.02) 0px, rgba(86, 86, 86, 0.02) 22px,rgba(202, 202, 202, 0.02) 22px, rgba(202, 202, 202, 0.02) 67px,rgba(247, 247, 247, 0.02) 67px, rgba(247, 247, 247, 0.02) 113px,rgba(135, 135, 135, 0.02) 113px, rgba(135, 135, 135, 0.02) 132px,rgba(157, 157, 157, 0.02) 132px, rgba(157, 157, 157, 0.02) 153px,rgba(53, 53, 53, 0.02) 153px, rgba(53, 53, 53, 0.02) 171px,rgba(17, 17, 17, 0.02) 171px, rgba(17, 17, 17, 0.02) 181px,rgba(179, 179, 179, 0.02) 181px, rgba(179, 179, 179, 0.02) 220px),repeating-linear-gradient(135deg, rgba(58, 58, 58, 0.02) 0px, rgba(58, 58, 58, 0.02) 41px,rgba(198, 198, 198, 0.02) 41px, rgba(198, 198, 198, 0.02) 60px,rgba(176, 176, 176, 0.02) 60px, rgba(176, 176, 176, 0.02) 99px,rgba(173, 173, 173, 0.02) 99px, rgba(173, 173, 173, 0.02) 146px,rgba(164, 164, 164, 0.02) 146px, rgba(164, 164, 164, 0.02) 167px,rgba(179, 179, 179, 0.02) 167px, rgba(179, 179, 179, 0.02) 205px,rgba(228, 228, 228, 0.02) 205px, rgba(228, 228, 228, 0.02) 230px,rgba(23, 23, 23, 0.02) 230px, rgba(23, 23, 23, 0.02) 241px),repeating-linear-gradient(135deg, rgba(190, 190, 190, 0.02) 0px, rgba(190, 190, 190, 0.02) 15px,rgba(74, 74, 74, 0.02) 15px, rgba(74, 74, 74, 0.02) 45px,rgba(98, 98, 98, 0.02) 45px, rgba(98, 98, 98, 0.02) 71px,rgba(43, 43, 43, 0.02) 71px, rgba(43, 43, 43, 0.02) 95px,rgba(131, 131, 131, 0.02) 95px, rgba(131, 131, 131, 0.02) 118px,rgba(21, 21, 21, 0.02) 118px, rgba(21, 21, 21, 0.02) 130px,rgba(77, 77, 77, 0.02) 130px, rgba(77, 77, 77, 0.02) 167px,rgba(231, 231, 231, 0.02) 167px, rgba(231, 231, 231, 0.02) 189px),linear-gradient(90deg, rgb(251, 251, 251),rgb(250, 250, 250))" >
 	<?php include("header.php"); ?>
@@ -58,7 +58,7 @@ $var=('<iframe src="https://maps.google.com/maps?q='.$json_file[$arval]['name'].
 	</div>
 	<div class="container px-4">
   		<div class="row gx-5">
-    		<div class="col mapy" style=" border: 10px solid black">
+    		<div class="col" style=" border: 10px solid black">
 					<!---<div style="display: inline-block;">
 					<div id="map" style="float:left; margin:50px; border: 10px solid black; box-shadow: 2px;">-->
 						<?php  echo($var);?>
