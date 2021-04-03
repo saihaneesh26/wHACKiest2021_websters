e9d97ab9fc1e1a8f8b21dd9643bb34aab3573a64
saihaneesh26 <56464193+saihaneesh26@users.noreply.github.com>
2021-04-03 12:34:08 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <!DOCTYPE html>
 <html>
   <html>
-  <head>
+  <head> 
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="style.css">
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
+    
+      
       google.charts.load('current', {'packages':['corechart']});
       google.charts.setOnLoadCallback(drawChart);
 
@@ -77,21 +83,68 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet">
   </head>
 
 
-  <body style="background-image:url();background-color: black;background-size: cover;background-position:center;background-repeat: no-repeat;">
+  <body style=" font-family: 'Poppins', sans-serif !important; background-image: repeating-linear-gradient(135deg, rgba(86, 86, 86, 0.02) 0px, rgba(86, 86, 86, 0.02) 22px,rgba(202, 202, 202, 0.02) 22px, rgba(202, 202, 202, 0.02) 67px,rgba(247, 247, 247, 0.02) 67px, rgba(247, 247, 247, 0.02) 113px,rgba(135, 135, 135, 0.02) 113px, rgba(135, 135, 135, 0.02) 132px,rgba(157, 157, 157, 0.02) 132px, rgba(157, 157, 157, 0.02) 153px,rgba(53, 53, 53, 0.02) 153px, rgba(53, 53, 53, 0.02) 171px,rgba(17, 17, 17, 0.02) 171px, rgba(17, 17, 17, 0.02) 181px,rgba(179, 179, 179, 0.02) 181px, rgba(179, 179, 179, 0.02) 220px),repeating-linear-gradient(135deg, rgba(58, 58, 58, 0.02) 0px, rgba(58, 58, 58, 0.02) 41px,rgba(198, 198, 198, 0.02) 41px, rgba(198, 198, 198, 0.02) 60px,rgba(176, 176, 176, 0.02) 60px, rgba(176, 176, 176, 0.02) 99px,rgba(173, 173, 173, 0.02) 99px, rgba(173, 173, 173, 0.02) 146px,rgba(164, 164, 164, 0.02) 146px, rgba(164, 164, 164, 0.02) 167px,rgba(179, 179, 179, 0.02) 167px, rgba(179, 179, 179, 0.02) 205px,rgba(228, 228, 228, 0.02) 205px, rgba(228, 228, 228, 0.02) 230px,rgba(23, 23, 23, 0.02) 230px, rgba(23, 23, 23, 0.02) 241px),repeating-linear-gradient(135deg, rgba(190, 190, 190, 0.02) 0px, rgba(190, 190, 190, 0.02) 15px,rgba(74, 74, 74, 0.02) 15px, rgba(74, 74, 74, 0.02) 45px,rgba(98, 98, 98, 0.02) 45px, rgba(98, 98, 98, 0.02) 71px,rgba(43, 43, 43, 0.02) 71px, rgba(43, 43, 43, 0.02) 95px,rgba(131, 131, 131, 0.02) 95px, rgba(131, 131, 131, 0.02) 118px,rgba(21, 21, 21, 0.02) 118px, rgba(21, 21, 21, 0.02) 130px,rgba(77, 77, 77, 0.02) 130px, rgba(77, 77, 77, 0.02) 167px,rgba(231, 231, 231, 0.02) 167px, rgba(231, 231, 231, 0.02) 189px),linear-gradient(90deg, rgb(251, 251, 251),rgb(250, 250, 250));">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
     
-    <?php include "header.php" ?>   
-    <div style="background-color: black;opacity: 70%;background-color: transparent;">
-    <div class = "container-fluid">
-      <div id="curve_chart" style="width: 700px; height: 500px ;float:left;padding:0;margin-top:100px"></div>
-    </div>
-    <div class = "container-fluid">
-      <div id="ncurve_chart" style="width: 700px; height: 500px; float: right;padding: 0;margin-top:100px"> </div>
-    </div>
+    <?php include"header.php"; ?>
+    <div class = "pagebody">
+        <div class="textmessage">
+            <h1>Lets Get Vaccinated.</h1>
+        </div>
+        <div class = "image">
+            
+            <div class="info">
+            <h3 style = "font-size: 40px; font-weight: 600;">What is Vaccine</h3>
+            <p>
+                A vaccine activates our immune system without making us sick. Many dangerous infectious diseases can be prevented in this simple and effective way.
+            </p>
+        </div>
+        <img src="v.jpg" alt="img">
+        </div>
+        <div class="spacers">
+
+        </div>
+
+        <div class = "image">
+            <div class="righty">
+            <img class = "right" src="v2.jpeg" alt="img">
+        </div>
+            <div class="info-r">
+            <h3 style = "font-size: 40px; font-weight: 600;">Importance of Vaccination</h3>
+            <p>
+                When we vaccinate, we activate the immune system's "memory." During vaccination, a weakened microbe, a fragment, or something that resembles it, is added to the body. The immune system is then activated without us becoming sick.</p>
+        </div>
+        
+        </div>
+
+        <div class="spacers">
+            
+        </div>
+
+        <div class = "image">
+            
+            <div class="info">
+            <h3 style = "font-size: 40px; font-weight: 600;">How are Vaccines given??</h3>
+            <p>
+                The rotavirus vaccine is given orally (drinkable vaccine). The other vaccines are administered by injection. Mercury is not used as a preservative in any of the vaccines in the Childhood Immunisation Programme.
+            </p>
+        </div>
+        <img  src="v3.jfif" alt="img">
+        </div>
+
+        <div class="spacers">
+            
+        </div>
+
 <div>
+
 </body>
+<?php include"footer.php"; ?>
 </html>
+
+<!-- style="background-color: black;opacity: 70%;background-color: transparent;"  -->
