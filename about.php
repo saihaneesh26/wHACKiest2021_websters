e9ddc40e4fb663faf293a249d529fb6d2a4d31da
saihaneesh26 <56464193+saihaneesh26@users.noreply.github.com>
2021-04-03 14:08:18 +0530

    Add files via upload

--- about.php
+++ about.php
@@ -7,8 +7,21 @@
 	<title>About</title>
 	<link rel="icon" href="favicon.ico" type="image/ico" >
 </head>
-<body>
-
-
+<body style="background-image: repeating-linear-gradient(135deg, rgba(86, 86, 86, 0.02) 0px, rgba(86, 86, 86, 0.02) 22px,rgba(202, 202, 202, 0.02) 22px, rgba(202, 202, 202, 0.02) 67px,rgba(247, 247, 247, 0.02) 67px, rgba(247, 247, 247, 0.02) 113px,rgba(135, 135, 135, 0.02) 113px, rgba(135, 135, 135, 0.02) 132px,rgba(157, 157, 157, 0.02) 132px, rgba(157, 157, 157, 0.02) 153px,rgba(53, 53, 53, 0.02) 153px, rgba(53, 53, 53, 0.02) 171px,rgba(17, 17, 17, 0.02) 171px, rgba(17, 17, 17, 0.02) 181px,rgba(179, 179, 179, 0.02) 181px, rgba(179, 179, 179, 0.02) 220px),repeating-linear-gradient(135deg, rgba(58, 58, 58, 0.02) 0px, rgba(58, 58, 58, 0.02) 41px,rgba(198, 198, 198, 0.02) 41px, rgba(198, 198, 198, 0.02) 60px,rgba(176, 176, 176, 0.02) 60px, rgba(176, 176, 176, 0.02) 99px,rgba(173, 173, 173, 0.02) 99px, rgba(173, 173, 173, 0.02) 146px,rgba(164, 164, 164, 0.02) 146px, rgba(164, 164, 164, 0.02) 167px,rgba(179, 179, 179, 0.02) 167px, rgba(179, 179, 179, 0.02) 205px,rgba(228, 228, 228, 0.02) 205px, rgba(228, 228, 228, 0.02) 230px,rgba(23, 23, 23, 0.02) 230px, rgba(23, 23, 23, 0.02) 241px),repeating-linear-gradient(135deg, rgba(190, 190, 190, 0.02) 0px, rgba(190, 190, 190, 0.02) 15px,rgba(74, 74, 74, 0.02) 15px, rgba(74, 74, 74, 0.02) 45px,rgba(98, 98, 98, 0.02) 45px, rgba(98, 98, 98, 0.02) 71px,rgba(43, 43, 43, 0.02) 71px, rgba(43, 43, 43, 0.02) 95px,rgba(131, 131, 131, 0.02) 95px, rgba(131, 131, 131, 0.02) 118px,rgba(21, 21, 21, 0.02) 118px, rgba(21, 21, 21, 0.02) 130px,rgba(77, 77, 77, 0.02) 130px, rgba(77, 77, 77, 0.02) 167px,rgba(231, 231, 231, 0.02) 167px, rgba(231, 231, 231, 0.02) 189px),linear-gradient(90deg, rgb(251, 251, 251),rgb(250, 250, 250));">
+	<?php include 'header.php'; ?>
+<div class="row">
+		<div class="col-md-12" style="text-align: center;margin: 1rem auto;">
+			<h1>Who are we?</h1>
+			<p>The websters team includes I Muni Sai Haneesh,Himanshu Behl,Amith Shubhan from RIT currently pursuing 4th semester in ISE.</p>
+		</div>
+</div>
+<div class="row">
+		<div class="col-md-12" style="text-align: center;margin: 1rem auto;">
+			<h1>Project</h1>
+			<p>This WebApplication helps the user to find the vaccination centers around Bengluru and the distance to that place from the user's location. It also provides the Mail-remainder to Users who registered. This application uses simple UI that helps in easy interaction to the Users. </p>
+			<p>This project is done as a part of the CodeRIT-wHACKiest2021 event, the 36hours long hackathon. We have gained good experience in doing this project.We thank the whole CodeRIT team for interacting with us and solving our problems.</p>
+		</div>
+</div>
+<?php include 'footer.php'; ?>
 </body>
 </html>
