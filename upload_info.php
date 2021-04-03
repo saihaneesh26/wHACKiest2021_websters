0145cd96e88f4945385fc6a904d417d56c7fb80b
Himanshu <76443309+himanshu-dxm@users.noreply.github.com>
2021-04-03 19:28:59 +0530

    made the form responsive

--- upload_info.php
+++ upload_info.php
@@ -36,8 +36,8 @@ else{
 
 <div class="container-fluid" style="margin: 2rem auto;">
 		<div class="row">
-			<div class="col-6" style="text-align: center;margin: 1rem auto;border:20px solid skyblue;padding-top:20px;">
-			<h4>Enter the vaccination center info</h4>	
+			<div class="col-12" style="text-align: center;margin: 1rem auto;border:20px solid skyblue;padding-top:20px;">
+			<h4 style="padding-bottom:10px;">Enter the vaccination center info</h4>	
 			<form class="form"method="POST" action="placeremainder.php">
 					<?php
 						if(isset($_SESSION['fail']))
@@ -52,25 +52,25 @@ else{
 						}
 					 ?>
 					<div class="form-group">
-                        <label for="name" style="color:coral">Name of hospital</label>
+                        <label for="name" style="color:coral" class="text-center">Name of Hospital:</label>
                         <input type="text" class="form-control" name="name" id="name" style="height:30px margin-block: 1rem;">
                     </div>
 					
 					<div class="form-group">
-                        <label for="vaccine_used"style="color:coral">vaccine name</label>
+                        <label for="vaccine_used"style="color:coral">Vaccine Name:</label>
                         <input type="text" class="form-control" name="vaccine_used" id="email" style="height:30px margin-block: 1rem;">
                     </div>
 					
 					<div class="form-group">
-  						<label for="price" style="color:coral">price of vaccine</label>
+  						<label for="price" style="color:coral">Price Of Vaccine:</label>
     					<input class="form-control" type="text"  name="price" id="price" style="height:30px margin-block: 1rem;">
 					</div>
 					<div class="form-group">
-  						<label for="lat" style="color:coral">latitiude</label>
+  						<label for="lat" style="color:coral">Latitiude:</label>
     					<input class="form-control" type="text"  name="lat" style="height:30px margin-block: 1rem;">
 					</div>
 					<div class="form-group">
-  						<label for="lon" style="color:coral">longitude</label>
+  						<label for="lon" style="color:coral">Longitude:</label>
     					<input class="form-control" type="text"  name="lon"style="height:30px margin-block: 1rem;">
 					</div>
 
