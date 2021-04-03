b359f2598f332bfc921b4b063424775cd399705e
saihaneesh26 <56464193+saihaneesh26@users.noreply.github.com>
2021-04-03 16:36:04 +0530

    Add files via upload

--- header.php
+++ header.php
@@ -27,7 +27,7 @@
 <body>  
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 
-    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: gray">
+    <nav class="navbar navbar-expand-lg navbar-light" >
       <div class="container-fluid">  
       <a class="navbar-brand" href="#" style ="font-family: 'Dancing Script', cursive;">Vaccinaid</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -49,6 +49,9 @@
             </li>
             <li class="nav" >
               <a class="nav-link active"  href="placeremainder.php">Reminder</a>
+            </li>
+            <li class="nav" >
+              <a class="nav-link active"  href="about.php">About Us</a>
             </li>
           </ul>
         </div>
